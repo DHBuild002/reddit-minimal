70028863dc535085d10ac99ce6ca0d037135bd1b
Dan Haddock <dhbuild.002@gmail.com>
2024-12-12 23:02:04 +0000

    Styling: Lay out basic shape of the mobile first app - Heading and Viewport area

--- src/App.js
+++ src/App.js
@@ -3,17 +3,17 @@ import Home from "./pages/Home";
 import Messages from "./pages/Messages";
 import Settings from "./pages/Settings";
 
-import "./App.css";
-
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/messages" element={<Messages />} />
-        <Route path="/settings" element={<Settings />} />
-      </Routes>
-    </Router>
+    <div className="flex justify-center">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/messages" element={<Messages />} />
+          <Route path="/settings" element={<Settings />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,14 @@
 export default function Home() {
   return (
-    <div className="p-4">Welcome to Reddit Mini - Like Reddit, but Mini</div>
+    <>
+      <div className="justify-center mx-auto w-1/2">
+        <div className="p-5 shadow-md">
+          <h2 className="text-center font-bold text-2xl">
+            REDDIT <span className="tracking-widest italic">Mini</span>
+          </h2>
+        </div>
+        <div className="h-screen shadow-md bg-white"></div>
+      </div>
+    </>
   );
 }
