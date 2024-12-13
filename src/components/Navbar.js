943400091e55e94ff25741130507c33ad5b82b84
Dan Haddock <dhbuild.002@gmail.com>
2024-12-13 21:11:36 +0000

    FIX: Icons now work with after pseudo attribute - Style buttons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import "./styles/navbar.css";
 
 const Navbar = () => {
   return (
-    <ul className="nav flex justify-evenly w-full list-none bottom-0 h-12 left-0">
+    <ul className="nav relative flex justify-evenly w-full list-none bottom-0 h-12 left-00">
       <li className="home"></li>
       <li className="messages"></li>
       <li className="settings"></li>
