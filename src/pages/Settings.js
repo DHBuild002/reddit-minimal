096b98ab1c2eef735aa6010929a0453dd4c65566
Dan Haddock <dhbuild.002@gmail.com>
2024-12-14 13:04:44 +0000

    ADD: Built out UI for Dark/Light Switcher in UI - WIP: Develop State functionality to update all app styling to be dark or light mode depending on this setting

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,9 +1,23 @@
 import { Link } from "react-router-dom";
+import "../components/styles/settings.css";
 
 export default function Settings() {
   return (
     <div className="p-4">
       <p>Account Settings</p>
+      <div className="ui-mode w-max p-5">
+        <h3 className="text-base flex-nowrap p-0">Dark/Light Switcher</h3>
+        <ul className="switch-menu">
+          <li className="list-item">
+            <Link to="/settings"></Link>
+          </li>
+
+          <li className="list-item">
+            <Link to="/settings"></Link>
+          </li>
+        </ul>
+      </div>
+
       <h4>
         <Link to="/">Back</Link>
       </h4>
