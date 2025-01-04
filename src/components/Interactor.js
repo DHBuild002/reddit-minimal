efd666092313d4e95b54d0f9fbb23d957962a583
Dan Haddock <dhbuild.002@gmail.com>
2025-01-04 19:12:19 +0000

    FIX: Apply alignment styling to interaction icons

--- src/components/Interactor.js
+++ src/components/Interactor.js
@@ -3,9 +3,11 @@ import React from "react";
 const Interactor = () => {
   return (
     <>
-      <div>Heart</div>
-      <div>GIFT</div>
-      <div>Share</div>
+      <div className="flex justify-center flex-row gap-6 mt-10">
+        <div className="icon">Heart</div>
+        <div className="icon">GIFT</div>
+        <div className="icon">Share</div>
+      </div>
     </>
   );
 };
