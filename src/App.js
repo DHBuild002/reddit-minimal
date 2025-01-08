cd3f9a1fdd35068ecb891ea4ce82a98ec0729e19
Dan Haddock <dhbuild.002@gmail.com>
2025-01-08 19:00:58 +0000

    FIX: Remove some commented code no longer used for API requests

--- src/App.js
+++ src/App.js
@@ -12,32 +12,6 @@ import { selectThemeMode } from "./state/themeSlice";
 
 function App() {
   // const mode = useSelector(selectThemeMode);
-  // const dispatch = useDispatch();
-  // const { username, accessToken } = useSelector((state) => state.auth);
-  // const urlParams = new URLSearchParams(window.location.search);
-  // const code = urlParams.get("code");
-
-  // // Auth User account
-  // useEffect(() => {
-  //   if (code) {
-  //     dispatch(
-  //       getAccessToken(code).then((action) => {
-  //         if (getAccessToken.fulfilled.match(action)) {
-  //           fetch("https://oauth.reddit.com/api/v1/me", {
-  //             headers: {
-  //               Authorization: `Bearer ${action.payload.access_token}`,
-  //             },
-  //           })
-  //             .then((res) => res.json())
-  //             .then((data) => dispatch(setUserInfo(data.name)));
-  //         }
-  //       })
-  //     );
-  //     window.history.pushState({}, document.title, "/"); //Clear URL
-  //   }
-  // }, [code, dispatch]);
-
-  // // Update Dark/Light mode
   // useEffect(() => {
   //   document.documentElement.setAttribute("data-theme", mode);
   // }, [mode]);
