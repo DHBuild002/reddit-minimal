ede2ca7f6007b717eed9e09459c595587dde7fdb
Dan Haddock <dhbuild.002@gmail.com>
2025-01-11 13:36:31 +0000

    FIX: Improve interaction button styling

--- src/components/Interactor.js
+++ src/components/Interactor.js
@@ -8,13 +8,13 @@ const Interactor = () => {
       <div className="flex justify-center flex-row gap-6 mt-10">
         <ul className="flex flex-row gap-4">
           <Link to="#heart">
-            <li className="int gift"></li>
+            <li className="int gift" id="item-hover"></li>
           </Link>
           <Link to="#gift">
-            <li className="int share"></li>
+            <li className="int share" id="item-hover"></li>
           </Link>
           <Link to="#share">
-            <li className="int like"></li>
+            <li className="int like" id="item-hover"></li>
           </Link>
         </ul>
       </div>
